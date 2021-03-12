1b664a3f8df403fb460b64f39abb682743f14133
Shubham Gupta <gupta.shubham823@gmail.com>
2021-03-12 15:26:19 +0530

    second commit

--- install.groovy
+++ install.groovy
@@ -1,5 +1,41 @@
-job('demo') {
-    steps {
-        shell('echo Hello World!')
+pipelineJob('job-name') {
+  definition {
+    cps {
+      script('''
+        pipeline {
+            agent any
+                stages {
+                    stage('Setup parameters') {
+                    steps {
+                        script { 
+                             properties([
+                                parameters([
+                                  string(
+                                defaultValue: '192.168.29.200', 
+                                name: 'target-host', 
+                                trim: true
+                            )
+                        ])
+                    ])  
+                       }
+                       }
+                       }  
+                       stage('Stage clone the repository') {
+                        steps {
+                                 git 'https://github.com/shubham823/play.git'
+                            }
+                    }
+                    stage('Stage playbook') {
+                        steps {
+                            ansiblePlaybook credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', installation: 'ansible', inventory: 'hosts', playbook: 'installTomcat.yml'
+                        }
+                    }
+                
+                }
+            }
+        }
+      '''.stripIndent())
+      sandbox()     
     }
+  }
 }
