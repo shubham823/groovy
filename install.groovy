70a990bce603e027ff7b39647ffd381e27c6cf93
Shubham Gupta <gupta.shubham823@gmail.com>
2021-03-12 16:13:56 +0530

    fifth commit

--- install.groovy
+++ install.groovy
@@ -1,34 +1,36 @@
-pipelineJob('check pipeline DSL') {
+pipelineJob('job-name') {
+    parameters {
+        stringParam('target_host', '192.168.29.200', 'ip off target_host')
+    }
   definition {
     cps {
       script('''
         pipeline {
             agent any
                 stages {
-                    stage('Setup parameters') {
-                    steps {
-                        script { 
-                             properties([
-                                parameters([
-                                  string(
-                                defaultValue: '192.168.29.200', 
-                                name: 'target-host', 
-                                trim: true
-                            )
-                        ])
-                    ])  
-                       }
-                       }
-                       }  
+                     
                        stage('Stage clone the repository') {
                         steps {
                                  git 'https://github.com/shubham823/play.git'
                             }
                     }
                     stage('Stage playbook') {
-                        steps {
-                            ansiblePlaybook credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', installation: 'ansible', inventory: 'hosts', playbook: 'installTomcat.yml'
-                        }
+                        steps{
+             script {
+                 withCredentials([sshUserPrivateKey(credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', keyFileVariable: 'keyfile')]) {
+                    ansiblePlaybook(
+                        installation: 'ansible',
+                        playbook: 'installTomcat.yml',
+                        inventory: 'hosts',
+                        colorized: true,
+                         extraVars: [
+                             target_host: "${params.target_host}"
+                             ]
+                        ) 
+                 }
+             }
+    //         ansiblePlaybook credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', installation: 'ansible', inventory: 'hosts', playbook: 'installTomcat.yml'
+         }
                     }
                 
                 }
@@ -38,4 +40,5 @@ pipelineJob('check pipeline DSL') {
       sandbox()     
     }
   }
-}
+  }
+
