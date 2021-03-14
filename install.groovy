6cb7c28cfb6c49e50873b1d2b6b6aa14279036cd
shubham823 <gupta.shubham823@gmail.com>
2021-03-14 15:03:23 +0530

    sixth commit

--- install.groovy
+++ install.groovy
@@ -1,44 +1,8 @@
-pipelineJob('job-name') {
-    parameters {
-        stringParam('target_host', '192.168.29.200', 'ip off target_host')
-    }
+pipelineJob('seeded') {
   definition {
     cps {
-      script('''
-        pipeline {
-            agent any
-                stages {
-                     
-                       stage('Stage clone the repository') {
-                        steps {
-                                 git 'https://github.com/shubham823/play.git'
-                            }
-                    }
-                    stage('Stage playbook') {
-                        steps{
-             script {
-                 withCredentials([sshUserPrivateKey(credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', keyFileVariable: 'keyfile')]) {
-                    ansiblePlaybook(
-                        installation: 'ansible',
-                        playbook: 'installTomcat.yml',
-                        inventory: 'hosts',
-                        colorized: true,
-                         extraVars: [
-                             target_host: "${params.target_host}"
-                             ]
-                        ) 
-                 }
-             }
-    //         ansiblePlaybook credentialsId: '74ee0ed5-fa02-49f7-9580-1c92420ab346', installation: 'ansible', inventory: 'hosts', playbook: 'installTomcat.yml'
-         }
-                    }
-                
-                }
-            }
-        }
-      '''.stripIndent())
+      script(readFileFromWorkspace('Jenkinsfile'))
       sandbox()     
     }
   }
-  }
-
+}
