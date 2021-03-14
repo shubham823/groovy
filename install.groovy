463fd07246c11d8fdf68d6607a549b0ecc8e6b64
shubham823 <gupta.shubham823@gmail.com>
2021-03-14 15:06:54 +0530

    seventh commit

--- install.groovy
+++ install.groovy
@@ -1,4 +1,8 @@
 pipelineJob('seeded') {
+  
+   parameters {
+        stringParam('target_host', '192.168.29.200', 'ip off target_host')
+    }
   definition {
     cps {
       script(readFileFromWorkspace('Jenkinsfile'))
