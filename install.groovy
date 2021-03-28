ee83946b344670d6d515449d0d5a83005505d675
shubham823 <gupta.shubham823@gmail.com>
2021-03-29 01:03:45 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -1,11 +1,12 @@
 pipelineJob('seeded') {
-   options { buildDiscarder(logRotator(numToKeepStr: '1')) }
+   
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
     }
   
   definition {
     cps {
+       options { buildDiscarder(logRotator(numToKeepStr: '1')) }
       script(readFileFromWorkspace('Jenkinsfile'))
       sandbox()     
     }
