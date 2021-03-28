705152d73b6cda1900ebb7bdb584380df48155ff
shubham823 <gupta.shubham823@gmail.com>
2021-03-28 23:45:27 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -1,11 +1,10 @@
 pipelineJob('seeded') {
-  definition {
-  properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '30')), [$class: 'ScannerJobProperty', doNotScan: false]])
+  
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
     }
   
-  
+  definition {
     cps {
       script(readFileFromWorkspace('Jenkinsfile'))
       sandbox()     
