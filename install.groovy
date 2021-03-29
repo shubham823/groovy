fd11ad7c89c939616ea9ba50d82ad78010e29563
shubham823 <gupta.shubham823@gmail.com>
2021-03-29 12:55:28 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -1,7 +1,7 @@
 pipelineJob('seeded') {
    logRotator {
-        numToKeep(5)
-        artifactNumToKeep(1)
+        numToKeep(30)
+       
     }
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
