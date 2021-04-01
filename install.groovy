7ef15215fc28d97ff979cc7e20bcb32ecd8e67dc
shubham823 <gupta.shubham823@gmail.com>
2021-04-01 15:01:13 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -5,6 +5,7 @@ pipelineJob('seeded') {
     }
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
+         stringParam('branch', null, 'feature branch')
     }
   
   definition {
