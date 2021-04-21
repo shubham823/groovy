cf9b41248c7b9f6cf2dd5fff6e9a570a2a063167
shubham823 <gupta.shubham823@gmail.com>
2021-04-21 16:53:37 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -5,10 +5,9 @@ pipelineJob('seeded') {
         numToKeep(30)
        
     }
-     
-     wrappers {
-        timeout()
-    }
+     timeout {
+            absolute(60)
+        }
      concurrentBuild(false)
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
