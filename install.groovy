7a29d975a64aac6fd45be2ba4b3f9c3574077728
shubham823 <gupta.shubham823@gmail.com>
2021-04-21 16:55:20 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -5,9 +5,7 @@ pipelineJob('seeded') {
         numToKeep(30)
        
     }
-     timeout {
-            absolute(60)
-        }
+    
      concurrentBuild(false)
    parameters {
         stringParam('target_host', '192.168.29.200', 'ip off target_host')
@@ -20,7 +18,9 @@ pipelineJob('seeded') {
           git{
              branch("*/master")
              remote{
-                url('https://github.com/shubham823/groovy.git')
+                url('https://github.com/shubham823/groovy.git'){
+                   timeout(4000)
+                }
                 
              }
              
